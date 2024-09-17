56a094f58101287dcdbe3ca36151c27e9d8262b4
YTDavid93 <ytd3809@gmail.com>
2024-09-17 21:14:20 +0545

    mongodb connection code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express, { Express } from "express";
 import cors from "cors";
 import routerHome from "../routes/home";
 import questionRouter from "../routes/questions";
+import mongoose from "mongoose";
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost/chatbot")
+  .then(() => console.log("Connected to databse"))
+  .catch((err) => console.log("Couldn't connect to Mongodb", err));
 
 const home = routerHome;
 const questions = questionRouter;
