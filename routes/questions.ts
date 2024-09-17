b02e537f223d29815d604c9329164f4116d1134b
YTDavid93 <ytd3809@gmail.com>
2024-09-17 21:15:18 +0545

    save quesiton and response in database

--- routes/questions.ts
+++ routes/questions.ts
@@ -1,21 +1,27 @@
 import axios from "axios";
 import express, { Request, Response } from "express";
+import { Interaction } from "../models/question";
 
 const questionRouter = express.Router();
 const flaskApiUrl = process.env.FLASK_API_URL || "http://127.0.0.1:5000/predict";
 
 questionRouter.post("/", async (req: Request, res: Response) => {
-  const { question } = req.body;
+  const { question } = req.body; 
 
   if (!question) return res.status(400).json({ error: "Question is required" });
 
   try {
     const response = await axios.post(flaskApiUrl, { question });
+
+    await Interaction.create({
+      question,
+      response: response.data.answer, 
+    }
+  );
+
     return res.json(response.data);
   } catch (error) {
-    return res
-      .status(500)
-      .json({ error: "Failed to get a response from the Python API" });
+    return res.status(500).json({ error: "Failed to get a response from the Python API" });
   }
 });
 
