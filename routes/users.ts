383fdb0b399484c6b6aaba78028bc5efcd7d3f6f
YTDavid93 <ytd3809@gmail.com>
2024-09-19 09:32:50 +0545

    setting response header

--- routes/users.ts
+++ routes/users.ts
@@ -2,25 +2,27 @@ import express, { Request, Response } from "express";
 import { User, validateUser } from "../models/user";
 import _ from "lodash";
 import bcrypt from "bcrypt";
+import jsonwentoken from "jsonwebtoken";
+import config from "config";
 
 const userRouter = express.Router();
 
 userRouter.post("/", async (req: Request, res: Response) => {
-    const { error } =  validateUser(req.body);
+  const { error } = validateUser(req.body);
 
-    if(error) return res.status(400).json({ errors: error.errors });
+  if (error) return res.status(400).json({ errors: error.errors });
 
-    let user = await User.findOne({ email: req.body.email }); // returns a promise we await it
-    if (user) return res.status(400).send("User already registered");
+  let user = await User.findOne({ email: req.body.email }); // returns a promise we await it
+  if (user) return res.status(400).send("User already registered");
 
-    user = new User(_.pick(req.body, ['name', 'email', 'password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
 
-    await user.save();
+  const token = jsonwentoken.sign({ _id: user._id }, config.get("jwtPrivateKey"));
 
-    res.send(_.pick(user, ['_id', 'name', 'email']));
+  res.header('x-auth-token', token).send(_.pick(user, ["_id", "name", "email"]));
 });
-
 
 export default userRouter;
