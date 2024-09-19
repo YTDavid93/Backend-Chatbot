94ab4cd071715af8d7d8319ded68ef77816f4ea0
YTDavid93 <ytd3809@gmail.com>
2024-09-19 14:12:30 +0545

    change in error handling

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,15 +2,20 @@ import { Request, Response, NextFunction } from "express";
 import jsonwebtoken from "jsonwebtoken";
 import config from "config";
 
-const auth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.header("x-auth-toke");
+const auth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided");
 
   try {
     const decoded = jsonwebtoken.verify(token, config.get("jwtPrivateKey"));
-    req.user = decoded;
-  } catch (ex){ 
-    res.status(400).send('Invalid Token');
+    req.user = decoded
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid Token");
   }
 };
 
