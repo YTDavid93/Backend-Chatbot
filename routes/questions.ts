2c0b630f082569623cc301eaa429661cba37aa26
YTDavid93 <ytd3809@gmail.com>
2024-09-22 19:49:54 +0545

    Logging an interaction

--- routes/questions.ts
+++ routes/questions.ts
@@ -1,31 +1,41 @@
 import axios from "axios";
 import express, { Request, Response } from "express";
 import { Interaction } from "../models/question";
-import auth from "../middleware/auth"
+import { User } from "../models/user"
+import auth from "../middleware/auth";
 
 const questionRouter = express.Router();
 const flaskApiUrl = process.env.FLASK_API_URL || "http://127.0.0.1:5000/predict";
 
 questionRouter.post("/", auth, async (req: Request, res: Response) => {
-  const { question } = req.body; 
+  const { userId, question } = req.body;
 
-  if (!question) return res.status(400).json({ error: "Question is required" });
+  if (!userId || !question) return res.status(400).json({ error: "Question and UserId is requied" });
 
   try {
-    const response = await axios.post(
-      flaskApiUrl,
-      { question },
-    );
+    const response = await axios.post(flaskApiUrl, { question });
 
-    await Interaction.create({
-      question,
-      response: response.data.answer, 
-    });
+    await User.findOneAndUpdate(
+      { _id: userId },
+      {
+        $push: {
+          interactions: {
+            question,
+            response: response.data.answer,
+            createdAt: new Date(),
+          },
+        },
+      },
+      { upsert: true, new: true }
+    );
 
     return res.json(response.data);
   } catch (error) {
-    return res.status(500).json({ error: "Failed to get a response from the Python API" });
+    return res
+      .status(500)
+      .json({ error: "Failed to get a response from the Python API" });
   }
+
 });
 
 export default questionRouter;
