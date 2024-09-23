1ce1099a1dbf7dff337f06845d4f85f566c9ca3d
YTDavid93 <ytd3809@gmail.com>
2024-09-23 12:02:07 +0545

    send interaction id in response

--- routes/questions.ts
+++ routes/questions.ts
@@ -1,21 +1,23 @@
 import axios from "axios";
 import express, { Request, Response } from "express";
 import { Interaction } from "../models/question";
-import { User } from "../models/user"
+import { User } from "../models/user";
 import auth from "../middleware/auth";
 
 const questionRouter = express.Router();
-const flaskApiUrl = process.env.FLASK_API_URL || "http://127.0.0.1:5000/predict";
+const flaskApiUrl =
+  process.env.FLASK_API_URL || "http://127.0.0.1:5000/predict";
 
 questionRouter.post("/", auth, async (req: Request, res: Response) => {
   const { userId, question } = req.body;
 
-  if (!userId || !question) return res.status(400).json({ error: "Question and UserId is requied" });
+  if (!userId || !question)
+    return res.status(400).json({ error: "Question and UserId is requied" });
 
   try {
     const response = await axios.post(flaskApiUrl, { question });
 
-    await User.findOneAndUpdate(
+    const updatedUser = await User.findOneAndUpdate(
       { _id: userId },
       {
         $push: {
@@ -29,13 +31,14 @@ questionRouter.post("/", auth, async (req: Request, res: Response) => {
       { upsert: true, new: true }
     );
 
-    return res.json(response.data);
+    const lastInteraction = updatedUser.interactions[updatedUser.interactions.length - 1];
+
+    return res.json({ answer: response.data.answer, interactionId: lastInteraction?._id });
   } catch (error) {
     return res
       .status(500)
       .json({ error: "Failed to get a response from the Python API" });
   }
-
 });
 
 export default questionRouter;
